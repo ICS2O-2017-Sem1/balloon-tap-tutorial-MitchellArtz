7e48470ef9bd97b81e446e17cf8c651ce779dcda
MitchellArtz <mitchell.artz@ocsbstudent.ca>
2017-09-19 10:45:03 -0400

    Fixed bug
    Fixed a bug with "balloon" spelt as "ballon" resulting in a crash

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ background.x = display.contentCenterX
 background.y = display.contentCenterY
 
 local tapText = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 40)
-
+tapText:setFillColor( 0, 0, 0 )
 local platform = display.newImageRect( "platform.png", 300, 50 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight-25
@@ -19,7 +19,7 @@ platform.y = display.contentHeight-25
 local balloon = display.newImageRect( "balloon.png", 112, 112 )
 balloon.x = display.contentCenterX
 balloon.y = display.contentCenterY
-ballon.alpha = 0.8
+balloon.alpha = 0.8
 
 local physics = require( "physics" )
 physics.start()
@@ -28,7 +28,7 @@ physics.addBody( platform, "static" )
 physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
      balloon:applyLinearImpulse( 0, -0.75, balloon.x, balloon.y )
 local function pushBalloon()
-
+tapCount = tapCount
 end
 
 balloon:addEventListener( "tap", pushBalloon)
