af041efe387aeedadbd74441da42a3295674eb97
MitchellArtz <mitchell.artz@ocsbstudent.ca>
2017-09-19 10:36:51 -0400

    Added Tap counter v1
    Added "local tapCount = 0" to the first line

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@
 --
 -----------------------------------------------------------------------------------------
 
+local tapCount = 0
+
 local background = display.newImageRect( "background.png", 360, 570 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
